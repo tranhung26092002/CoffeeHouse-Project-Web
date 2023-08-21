139edc42a62c6234f7ec9fcfb93bfbefa5fe4071
TranHung2609 <hungtran01686527618@gmail.com>
2023-08-21 23:40:55 +0700

    first commit

---
+++ client/js/home_page.js
@@ -0,0 +1,43 @@
+function handleTrangchu(){
+    window.location.href = '/home_page.html';
+}
+
+function handleFood(){
+    window.location.href = '/doanvat.html';
+}
+
+function handleDrink(){
+    window.location.href = '/drink_page.html';
+}
+
+function handleBill(){
+    window.location.href = '/thanhtoan.html';
+}
+
+function handleNews(){
+    window.location.href = '/tintuc.html';
+}
+
+function handleAddress(){
+    window.location.href = '/diachi.html';
+}
+
+function handleLogoutUser(){
+    localStorage.removeItem('access_token');
+    window.location.href = '/login.html';
+}
+
+async function getListTask(){
+    try {
+        // call api get listuser
+        const name = await axios.get('auth/admin/user');
+        //show name
+        const accessToken = localStorage.getItem('access_token')
+        const payloadDecoded = jwt_decode(accessToken);
+        document.querySelector('.name_user').innerText = payloadDecoded.username;
+
+    } catch (error) {
+        //error
+    }
+}
+getListTask();
